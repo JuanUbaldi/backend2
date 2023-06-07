aa54ee83c5ec8bddc8cfb0a905cccf7c9e6577e5
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-06-07 16:08:27 -0300

    commit

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,54 +1,57 @@
-import { ProductModel } from "../dao/models/products.model.js";
+import { ProductModel } from "../dao/models/products.models.js";
 
 class ProductService {
-    async getAllProducts() {
-        try {
-            const products = await ProductModel.paginate({},{limit:5,page:1});
-            return products;/* .docs.map(product => product.toObject()); */
-        } catch (error) {
-            throw error;
-        }
+  async getAllProducts() {
+    try {
+      const products = await ProductModel.find({});
+      return products; /* .docs.map(product => product.toObject()); */
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getProductById(productId) {
-        try {
-            const product = await ProductModel.findById(productId);
-            return product;
-        } catch (error) {
-            throw error;
-        }
+  async getProductById(productId) {
+    try {
+      const product = await ProductModel.findById(productId);
+      return product;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async createProduct(productData) {
-        try {
-            const product = await ProductModel.create(productData);
-            return product;
-        } catch (error) {
-            throw error;
-        }
+  async createProduct(productData) {
+    try {
+      const product = await ProductModel.create(productData);
+      return product;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async updateProduct(productId, productData) {
-        try {
-            const product = await ProductModel.findByIdAndUpdate(
-                productId,
-                productData,
-                { new: true }
-            );
-            return product;
-        } catch (error) {
-            throw error;
-        }
+  async updateProduct(productId, productData) {
+    try {
+      if (!productId) {
+        throw new Error("invalid id");
+      }
+      const product = await ProductModel.findByIdAndUpdate(
+        productId,
+        productData
+        /* { new: true } */
+      );
+      return product;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async deleteProduct(productId) {
-        try {
-            const product = await ProductModel.findByIdAndDelete(productId);
-            return product;
-        } catch (error) {
-            throw error;
-        }
+  async deleteProduct(productId) {
+    try {
+      const product = await ProductModel.findByIdAndDelete(productId);
+      return product;
+    } catch (error) {
+      throw error;
     }
-};
+  }
+}
 
 export default ProductService;
